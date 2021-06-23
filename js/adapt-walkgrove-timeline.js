6cee1d274e1a500e60b75d90cf37ada4661714b9
@laura.lululand <laura.lululand@gmail.com>
2021-06-23 17:43:29 +0100

    ability to show one item at a time i.e for audio

--- js/adapt-walkgrove-timeline.js
+++ js/adapt-walkgrove-timeline.js
@@ -6,6 +6,12 @@ define([
 
   var TimelineView = ComponentView.extend({
 
+    _timelineIndex: -1,
+
+    events: {
+      'click .js-timeline-more-click': 'onMoreClicked'
+    },
+    
     preRender: function() {
       this.checkIfResetOnRevisit();
     },
@@ -14,12 +20,69 @@ define([
       this.setReadyStatus();
 
       this.setupInview();
+
+      if(this.model.get( 'buttonMore' )) {
+
+        this.$('.timeline__button-more').addClass('is-visible');
+
+        this.$('.timeline__items').addClass('is-hidden');
+        this.$('.timeline__items').addClass('is-more');
+
+        this.model.get( '_items' ).forEach((date, index) => {
+          this.$('.timeline__widget').eq(index).addClass('is-hidden');
+        });
+
+      }
+
     },
+
+    onMoreClicked: function() {
+      var tempTimelineIndex = this._timelineIndex;
+      var nextTimelineDate = tempTimelineIndex++;
+
+      this.$('.timeline__items').removeClass('is-hidden');
+
+      if(nextTimelineDate < 0) {
+        nextTimelineDate = 0;
+      }
+      
+      if(this._timelineIndex < this.model.get( '_items' ).length) {
+        this._timelineIndex++;
+      }
+
+      this.model.get( '_items' ).forEach((date, index) => {
+        if(index <= this._timelineIndex) { //index > tempTimelineIndex && 
+          this.$('.timeline__widget').eq(index).removeClass('is-hidden');
+        }
+        if(index === this._timelineIndex) {
+          //audio?
+          if (Adapt.config.get('_sound')._isActive === true) {
+            console.log(date._audio);
+            if ( date._audio) {
+              Adapt.trigger('audio:partial', {src: date._audio._src});
+            }
+          }
+        }
+      });
+
+      //console.log(this._timelineIndex, this.model.get( '_items' ).length-1);
+
+      if(this._timelineIndex === this.model.get( '_items' ).length-1) {
+        this.$('.timeline__button-more').removeClass('is-visible');
+        this.setCompletionStatus();
+      }
+
+      this.$('#timeline__widget-' + nextTimelineDate + '').a11y_focus();
+      Adapt.scrollTo(this.$('#timeline__widget-' + nextTimelineDate + ''));
+
+      this.setupInview();
+    },
+
 
     setupInview: function() {
       var selector = this.getInviewElementSelector();
       if (!selector) {
-        this.setCompletionStatus();
+       // this.setCompletionStatus();
         return;
       }
 
